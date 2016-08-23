785fd21a6b6ad749f2e3f6fd9066887a38aad32d
shenderov <shenderov.k@gmail.com>
2016-08-23 14:09:54 +0300

    DateHelper

--- src/main/java/com/kamabizbazti/model/helpers/DateHelper.java
+++ src/main/java/com/kamabizbazti/model/helpers/DateHelper.java
@@ -4,6 +4,10 @@ import com.kamabizbazti.model.interfaces.IDateHelper;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.temporal.ChronoUnit;
 
 public class DateHelper implements IDateHelper{
 private static final SimpleDateFormat sdf = new SimpleDateFormat("MMMM");
@@ -61,4 +65,28 @@ private static final SimpleDateFormat sdf = new SimpleDateFormat("MMMM");
     public String getMonthNameByDate(long startDate) {
         return sdf.format(startDate);
     }
+    
+    public long weeksBetweenTwoDates(long startDate, long endDate) {
+       		Instant d1i = Instant.ofEpochMilli(startDate);
+ 		    Instant d2i = Instant.ofEpochMilli(endDate);
+ 		    LocalDateTime sDate = LocalDateTime.ofInstant(d1i, ZoneId.systemDefault());
+ 		    LocalDateTime eDate = LocalDateTime.ofInstant(d2i, ZoneId.systemDefault());
+ 		    return ChronoUnit.WEEKS.between(sDate, eDate);
+     }
+    
+    public long monthsBetweenTwoDates(long startDate, long endDate) {
+   		Instant d1i = Instant.ofEpochMilli(startDate);
+		    Instant d2i = Instant.ofEpochMilli(endDate);
+		    LocalDateTime sDate = LocalDateTime.ofInstant(d1i, ZoneId.systemDefault());
+		    LocalDateTime eDate = LocalDateTime.ofInstant(d2i, ZoneId.systemDefault());
+		    return ChronoUnit.MONTHS.between(sDate, eDate);
+ }
+    
+    public long yearsBetweenTwoDates(long startDate, long endDate) {
+   		Instant d1i = Instant.ofEpochMilli(startDate);
+		    Instant d2i = Instant.ofEpochMilli(endDate);
+		    LocalDateTime sDate = LocalDateTime.ofInstant(d1i, ZoneId.systemDefault());
+		    LocalDateTime eDate = LocalDateTime.ofInstant(d2i, ZoneId.systemDefault());
+		    return ChronoUnit.YEARS.between(sDate, eDate);
+ }
 }
