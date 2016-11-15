e0c24f78a2c34e846d71b3fabe7a0bb878e26686
shenderov <shenderov.k@gmail.com>
2016-11-15 22:11:58 +0200

    Security

--- src/main/java/com/kamabizbazti/config/KamaBizbaztiApplicationConfig.java
+++ src/main/java/com/kamabizbazti/config/KamaBizbaztiApplicationConfig.java
@@ -3,7 +3,10 @@ package com.kamabizbazti.config;
 import com.kamabizbazti.DataGenerator;
 import com.kamabizbazti.model.handlers.GeneralRequestHandler;
 import com.kamabizbazti.model.handlers.GeneralStatisticsHandler;
+import com.kamabizbazti.model.handlers.UserRequestHandler;
+import com.kamabizbazti.model.handlers.UserStatisticsHandler;
 import com.kamabizbazti.model.interfaces.IGeneralRequestHandler;
+import com.kamabizbazti.model.interfaces.IUserRequestHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,20 +14,29 @@ import org.springframework.context.annotation.Configuration;
 public class KamaBizbaztiApplicationConfig {
 
     @Bean
-    public IGeneralRequestHandler handler (){
+    public IGeneralRequestHandler handler() {
         return new GeneralRequestHandler();
     }
 
     @Bean
-    public DataGenerator dataGenerator (){
+    public IUserRequestHandler userRequestHandler() {
+        return new UserRequestHandler();
+    }
+
+    @Bean
+    public DataGenerator dataGenerator() {
         return new DataGenerator();
     }
 
     @Bean
-    public GeneralStatisticsHandler generalStatisticsHandler (){
+    public GeneralStatisticsHandler generalStatisticsHandler() {
         return new GeneralStatisticsHandler();
     }
 
+    @Bean
+    public UserStatisticsHandler userStatisticsHandler() {
+        return new UserStatisticsHandler();
+    }
 
 
 }
