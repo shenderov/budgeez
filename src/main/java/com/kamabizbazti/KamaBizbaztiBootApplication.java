a3dd5e9fcc58cc0c39982cff21599a948af14a3e
shenderov <shenderov.k@gmail.com>
2016-11-15 22:15:26 +0200

    Security

--- src/main/java/com/kamabizbazti/KamaBizbaztiBootApplication.java
+++ src/main/java/com/kamabizbazti/KamaBizbaztiBootApplication.java
@@ -2,9 +2,11 @@ package com.kamabizbazti;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-public class KamaBizbaztiBootApplication{
+public class KamaBizbaztiBootApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(KamaBizbaztiBootApplication.class, args);
