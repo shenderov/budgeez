964750e9b85f782c33c6ee7f50107b3ea0852a5e
shenderov <shenderov.k@gmail.com>
2016-11-16 08:40:28 +0200

    Security

--- src/main/java/com/kamabizbazti/restcontroller/AuthenticationRestController.java
+++ src/main/java/com/kamabizbazti/restcontroller/AuthenticationRestController.java
@@ -1,5 +1,11 @@
 package com.kamabizbazti.restcontroller;
 
+import com.kamabizbazti.security.entities.*;
+import com.kamabizbazti.model.repository.CurrencyRepository;
+import com.kamabizbazti.model.repository.LanguageRepository;
+import com.kamabizbazti.security.exceptions.EmailAlreadyRegistered;
+import com.kamabizbazti.security.repository.AuthorityRepository;
+import com.kamabizbazti.security.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
@@ -11,21 +17,20 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-import com.kamabizbazti.security.JwtAuthenticationRequest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
 import com.kamabizbazti.security.JwtTokenUtil;
-import com.kamabizbazti.security.JwtUser;
 import com.kamabizbazti.security.service.JwtAuthenticationResponse;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class AuthenticationRestController {
 
-    @Value("${jwt.header}")
+    @Value("${security.header}")
     private String tokenHeader;
 
     @Autowired
@@ -37,10 +42,27 @@ public class AuthenticationRestController {
     @Autowired
     private UserDetailsService userDetailsService;
 
-    @RequestMapping(value = "${jwt.route.authentication.path}", method = RequestMethod.POST)
-    public ResponseEntity<?> createAuthenticationToken(@RequestBody JwtAuthenticationRequest authenticationRequest, Device device) throws AuthenticationException {
+    @Autowired
+    private UserRepository userRepository;
 
-        // Perform the security
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private LanguageRepository languageRepository;
+
+    @Autowired
+    private CurrencyRepository currencyRepository;
+
+    @Autowired
+    private AuthorityRepository authorityRepository;
+
+    private static final String DEFAULT_LANGUAGE = "ENG";
+    private static final String DEFAULT_CURRENCY = "USD";
+
+    @CrossOrigin(origins = "*")
+    @RequestMapping(value = "${security.route.authentication.login}", method = RequestMethod.POST)
+    public ResponseEntity<?> createAuthenticationToken(@RequestBody JwtAuthenticationRequest authenticationRequest, Device device) throws AuthenticationException {
         final Authentication authentication = authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(
                         authenticationRequest.getUsername(),
@@ -48,21 +70,31 @@ public class AuthenticationRestController {
                 )
         );
         SecurityContextHolder.getContext().setAuthentication(authentication);
-
-        // Reload password post-security so we can generate token
         final UserDetails userDetails = userDetailsService.loadUserByUsername(authenticationRequest.getUsername());
         final String token = jwtTokenUtil.generateToken(userDetails, device);
-
-        // Return the token
         return ResponseEntity.ok(new JwtAuthenticationResponse(token));
     }
 
-    @RequestMapping(value = "${jwt.route.authentication.refresh}", method = RequestMethod.GET)
+    @CrossOrigin(origins = "*")
+    @RequestMapping(value = "${security.route.authentication.signup}", method = RequestMethod.POST)
+    public ResponseEntity<?> createUser(@RequestBody @Valid SignUpWrapper wrapper, Device device) throws EmailAlreadyRegistered {
+        if (userRepository.findByUsername(wrapper.getEmail()) != null)
+            throw new EmailAlreadyRegistered();
+        else {
+            User user = new User(wrapper.getEmail().toLowerCase(), passwordEncoder.encode(wrapper.getPassword()), wrapper.getName());
+            user.setAuthorities(setUserAuthorities());
+            user.setLanguage(languageRepository.findOne(DEFAULT_LANGUAGE));
+            user.setCurrency(currencyRepository.findOne(DEFAULT_CURRENCY));
+            userRepository.save(user);
+        }
+        return createAuthenticationToken(new JwtAuthenticationRequest(wrapper.getEmail().toLowerCase(), wrapper.getPassword()), device);
+    }
+
+    @RequestMapping(value = "${security.route.authentication.refresh}", method = RequestMethod.GET)
     public ResponseEntity<?> refreshAndGetAuthenticationToken(HttpServletRequest request) {
         String token = request.getHeader(tokenHeader);
         String username = jwtTokenUtil.getUsernameFromToken(token);
         JwtUser user = (JwtUser) userDetailsService.loadUserByUsername(username);
-
         if (jwtTokenUtil.canTokenBeRefreshed(token, user.getLastPasswordResetDate())) {
             String refreshedToken = jwtTokenUtil.refreshToken(token);
             return ResponseEntity.ok(new JwtAuthenticationResponse(refreshedToken));
@@ -71,4 +103,9 @@ public class AuthenticationRestController {
         }
     }
 
+    private List<Authority> setUserAuthorities() {
+        List<Authority> userAuthorities = new ArrayList<>();
+        userAuthorities.add(authorityRepository.findByName(AuthorityName.ROLE_USER));
+        return userAuthorities;
+    }
 }
--- src/main/java/com/kamabizbazti/restcontroller/RestControllerExceptionHandler.java
+++ src/main/java/com/kamabizbazti/restcontroller/RestControllerExceptionHandler.java
@@ -1,22 +1,77 @@
 package com.kamabizbazti.restcontroller;
 
+import com.kamabizbazti.security.exceptions.EmailAlreadyRegistered;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import javax.servlet.http.HttpServletRequest;
 
 @ControllerAdvice
-public class RestControllerExceptionHandler extends ResponseEntityExceptionHandler {
+public class RestControllerExceptionHandler {
+
+    private static final String generalServerError = "GENERAL_SERVER_ERROR";
+    private static final String invalidParameter = "INVALID_PARAMETER";
+    private static final String badCredentials = "INCORRECT_USERNAME_OR_PASSWORD";
+
+    @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR, reason = generalServerError)
     @ExceptionHandler({Exception.class})
     public ResponseEntity badRequest(HttpServletRequest req, Exception exception) {
-        //{"timestamp":1469985829212,"status":404,"error":"Not Found","message":
-        // "No message available","path":"/ProjectJava11a/getGeneralChartSelectionsList"}
         exception.printStackTrace();
         return ResponseEntity
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body(exception.getMessage());
+    }
+
+    @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR, reason = EmailAlreadyRegistered.message)
+    @ExceptionHandler({EmailAlreadyRegistered.class})
+    public ResponseEntity badRequest(HttpServletRequest req, EmailAlreadyRegistered exception) {
+        exception.printStackTrace();
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(exception.getMessage());
+    }
+
+    @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR, reason = invalidParameter)
+    @ExceptionHandler({MethodArgumentNotValidException.class})
+    public ResponseEntity badRequest(HttpServletRequest req, MethodArgumentNotValidException exception) {
+        exception.printStackTrace();
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(exception.getMessage());
+    }
+
+    @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR, reason = badCredentials)
+    @ExceptionHandler({BadCredentialsException.class})
+    public ResponseEntity badRequest(HttpServletRequest req, BadCredentialsException exception) {
+        exception.printStackTrace();
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(exception.getMessage());
+    }
+
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "UNIQUE_CONSTRAINT_VIOLATION")
+    @ExceptionHandler({DataIntegrityViolationException.class})
+    public ResponseEntity constraintViolation(HttpServletRequest req, DataIntegrityViolationException exception) {
+        exception.printStackTrace();
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body("UNIQUE_CONSTRAINT_VIOLATION");
+    }
+
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "INVALID_REQUEST_ENTITY")
+    @ExceptionHandler({HttpMessageNotReadableException.class})
+    public ResponseEntity integrityViolation(HttpServletRequest req, HttpMessageNotReadableException exception) {
+        exception.printStackTrace();
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body("INVALID_REQUEST_ENTITY");
+
     }
 }
