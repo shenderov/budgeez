cfdca514048f17249aa0742537ea987e0baaac06
shenderov <shenderov.k@gmail.com>
2016-11-18 20:35:02 +0200

    Improvements

--- src/main/java/com/kamabizbazti/model/helpers/DateHelper.java
+++ src/main/java/com/kamabizbazti/model/helpers/DateHelper.java
@@ -9,47 +9,44 @@ import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.temporal.ChronoUnit;
 
-public class DateHelper implements IDateHelper{
-private static final SimpleDateFormat sdf = new SimpleDateFormat("MMMM");
-private static final SimpleDateFormat SDF_MONTH_FULL = new SimpleDateFormat("MMMM");
-private static final SimpleDateFormat SDF_MONTH_SHORT = new SimpleDateFormat("MMM");
-private static final SimpleDateFormat SDF_MONTH_FULL_WEEK = new SimpleDateFormat("MMMM'/W'W");
-private static final SimpleDateFormat SDF_MONTH_SHORT_WEEK = new SimpleDateFormat("MMM'/W'W");
-private static final SimpleDateFormat SDF_YEAR = new SimpleDateFormat("YYYY");
+public class DateHelper implements IDateHelper {
 
-    @Override
+    private static final SimpleDateFormat SDF_MONTH_FULL = new SimpleDateFormat("MMMM");
+    private static final SimpleDateFormat SDF_MONTH_SHORT = new SimpleDateFormat("MMM");
+    private static final SimpleDateFormat SDF_MONTH_FULL_WEEK = new SimpleDateFormat("MMMM'/W'W");
+    private static final SimpleDateFormat SDF_MONTH_SHORT_WEEK = new SimpleDateFormat("MMM'/W'W");
+    private static final SimpleDateFormat SDF_MONTH_FULL_YEAR = new SimpleDateFormat("MMMM YYYY");
+    private static final SimpleDateFormat SDF_MONTH_SHORT_YEAR = new SimpleDateFormat("MMM YY");
+    private static final SimpleDateFormat SDF_YEAR = new SimpleDateFormat("YYYY");
+
+
     public long getFirstDayOfCurrentMonth() {
         return getFirsDayOfMonth(0);
     }
 
-    @Override
     public long getFirstDayOfPreviousMonth() {
         return getFirsDayOfMonth(1);
     }
 
-    @Override
     public long getFirstDayOfNMonthAgo(int monthAgo) {
         return getFirsDayOfMonth(monthAgo);
     }
 
-    @Override
     public long getLastDayOfCurrentMonth() {
         return getLastDayOfMonth(0);
     }
 
-    @Override
     public long getLastDayOfPreviousMonth() {
         return getLastDayOfMonth(1);
     }
 
-    @Override
     public long getLastDayOfNMonthAgo(int monthAgo) {
         return getLastDayOfMonth(monthAgo);
     }
 
-    private long getFirsDayOfMonth(int monthAgo) {
+    public long getFirsDayOfMonth(int monthAgo) {
         Calendar c = Calendar.getInstance();
-        c.add(Calendar.MONTH, - monthAgo);
+        c.add(Calendar.MONTH, -monthAgo);
         c.set(Calendar.DAY_OF_MONTH, c.getActualMinimum(Calendar.DAY_OF_MONTH));
         c.set(Calendar.HOUR_OF_DAY, 0);
         c.set(Calendar.MINUTE, 0);
@@ -57,9 +54,9 @@ private static final SimpleDateFormat SDF_YEAR = new SimpleDateFormat("YYYY");
         return c.getTimeInMillis();
     }
 
-    private long getLastDayOfMonth(int monthAgo) {
+    public long getLastDayOfMonth(int monthAgo) {
         Calendar c = Calendar.getInstance();
-        c.add(Calendar.MONTH, - monthAgo);
+        c.add(Calendar.MONTH, -monthAgo);
         c.set(Calendar.DAY_OF_MONTH, c.getActualMaximum(Calendar.DAY_OF_MONTH));
         c.set(Calendar.HOUR_OF_DAY, 23);
         c.set(Calendar.MINUTE, 59);
@@ -67,51 +64,77 @@ private static final SimpleDateFormat SDF_YEAR = new SimpleDateFormat("YYYY");
         return c.getTimeInMillis();
     }
 
-    public String getMonthNameByDate(long startDate) {
-        return sdf.format(startDate);
+    public long getFirsDayOfWeek(int weeksAgo) {
+        Calendar c = Calendar.getInstance();
+        c.add(Calendar.WEEK_OF_YEAR, -weeksAgo);
+        c.set(Calendar.WEEK_OF_MONTH, c.getActualMinimum(Calendar.WEEK_OF_MONTH));
+        c.set(Calendar.HOUR_OF_DAY, 0);
+        c.set(Calendar.MINUTE, 0);
+        c.set(Calendar.SECOND, 0);
+        System.out.println("getFirsDayOfWeek " + c.getTime());
+        return c.getTimeInMillis();
     }
-    
+
+    public long getLastDayOfWeek(int weeksAgo) {
+        Calendar c = Calendar.getInstance();
+        c.add(Calendar.WEEK_OF_YEAR, -weeksAgo);
+        c.set(Calendar.WEEK_OF_MONTH, c.getActualMaximum(Calendar.WEEK_OF_MONTH));
+        c.set(Calendar.HOUR_OF_DAY, 23);
+        c.set(Calendar.MINUTE, 59);
+        c.set(Calendar.SECOND, 59);
+        System.out.println("getLastDayOfWeek " + c.getTime());
+        return c.getTimeInMillis();
+    }
+
     public long weeksBetweenTwoDates(long startDate, long endDate) {
-       		Instant d1i = Instant.ofEpochMilli(startDate);
- 		    Instant d2i = Instant.ofEpochMilli(endDate);
- 		    LocalDateTime sDate = LocalDateTime.ofInstant(d1i, ZoneId.systemDefault());
- 		    LocalDateTime eDate = LocalDateTime.ofInstant(d2i, ZoneId.systemDefault());
- 		    return ChronoUnit.WEEKS.between(sDate, eDate);
-     }
-    
+        Instant d1i = Instant.ofEpochMilli(startDate);
+        Instant d2i = Instant.ofEpochMilli(endDate);
+        LocalDateTime sDate = LocalDateTime.ofInstant(d1i, ZoneId.systemDefault());
+        LocalDateTime eDate = LocalDateTime.ofInstant(d2i, ZoneId.systemDefault());
+        return ChronoUnit.WEEKS.between(sDate, eDate);
+    }
+
     public long monthsBetweenTwoDates(long startDate, long endDate) {
-   		Instant d1i = Instant.ofEpochMilli(startDate);
-		    Instant d2i = Instant.ofEpochMilli(endDate);
-		    LocalDateTime sDate = LocalDateTime.ofInstant(d1i, ZoneId.systemDefault());
-		    LocalDateTime eDate = LocalDateTime.ofInstant(d2i, ZoneId.systemDefault());
-		    return ChronoUnit.MONTHS.between(sDate, eDate);
- }
-    
+        Instant d1i = Instant.ofEpochMilli(startDate);
+        Instant d2i = Instant.ofEpochMilli(endDate);
+        LocalDateTime sDate = LocalDateTime.ofInstant(d1i, ZoneId.systemDefault());
+        LocalDateTime eDate = LocalDateTime.ofInstant(d2i, ZoneId.systemDefault());
+        return ChronoUnit.MONTHS.between(sDate, eDate);
+    }
+
     public long yearsBetweenTwoDates(long startDate, long endDate) {
-   		Instant d1i = Instant.ofEpochMilli(startDate);
-		    Instant d2i = Instant.ofEpochMilli(endDate);
-		    LocalDateTime sDate = LocalDateTime.ofInstant(d1i, ZoneId.systemDefault());
-		    LocalDateTime eDate = LocalDateTime.ofInstant(d2i, ZoneId.systemDefault());
-		    return ChronoUnit.YEARS.between(sDate, eDate);
- }
- 
- 	public String getFullMonthName(long date){
-		return SDF_MONTH_FULL.format(date);
-	}
-	
-	public String getShortMonthName(long date){
-		return SDF_MONTH_SHORT.format(date);
-	}
-	
-	public String getFullMonthWeek(long date){
-		return SDF_MONTH_FULL_WEEK.format(date);
-	}
-	
-	public String getShortMonthWeek(long date){
-		return SDF_MONTH_SHORT_WEEK.format(date);
-	}
-	
-	public String getYear(long date){
-		return SDF_YEAR.format(date);
-	}
+        Instant d1i = Instant.ofEpochMilli(startDate);
+        Instant d2i = Instant.ofEpochMilli(endDate);
+        LocalDateTime sDate = LocalDateTime.ofInstant(d1i, ZoneId.systemDefault());
+        LocalDateTime eDate = LocalDateTime.ofInstant(d2i, ZoneId.systemDefault());
+        return ChronoUnit.YEARS.between(sDate, eDate);
+    }
+
+    public String getFullMonthName(long date) {
+        return SDF_MONTH_FULL.format(date);
+    }
+
+    public String getShortMonthName(long date) {
+        return SDF_MONTH_SHORT.format(date);
+    }
+
+    public String getFullMonthWeek(long date) {
+        return SDF_MONTH_FULL_WEEK.format(date);
+    }
+
+    public String getShortMonthWeek(long date) {
+        return SDF_MONTH_SHORT_WEEK.format(date);
+    }
+
+    public String getYear(long date) {
+        return SDF_YEAR.format(date);
+    }
+
+    public String getFullMonthYear(long date) {
+        return SDF_MONTH_FULL_YEAR.format(date);
+    }
+
+    public String getShortMonthYear(long date) {
+        return SDF_MONTH_SHORT_YEAR.format(date);
+    }
 }
