8e469df20df42da997396685db2f1c9c8a06b4ea
shenderov <shenderov.k@gmail.com>
2016-12-12 18:59:13 +0200

    General API Tests

---
+++ src/main/java/com/kamabizbazti/model/interfaces/IGeneralRestController.java
@@ -0,0 +1,18 @@
+package com.kamabizbazti.model.interfaces;
+
+import com.kamabizbazti.model.entities.ChartRequestWrapper;
+import com.kamabizbazti.model.entities.ChartSelection;
+import com.kamabizbazti.model.entities.ChartWrapper;
+import com.kamabizbazti.model.exceptions.UnknownSelectionIdException;
+
+import java.util.List;
+
+public interface IGeneralRestController {
+    List<ChartSelection> getGeneralChartSelectionsList();
+
+    List<ChartSelection> getUserChartSelectionsList();
+
+    ChartWrapper getDefaultDataTable() throws Exception;
+
+    ChartWrapper getGeneralDataTable(ChartRequestWrapper chartRequestWrapper) throws UnknownSelectionIdException;
+}
