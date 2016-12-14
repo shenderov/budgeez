dee1354883e830c371a9d3dbcc88be480fbc6bbf
shenderov <shenderov.k@gmail.com>
2016-12-14 21:42:41 +0200

    test fix

--- src/test/java/com/kamabizbazti/KamaBizbaztiBootApplicationTests.java
+++ src/test/java/com/kamabizbazti/KamaBizbaztiBootApplicationTests.java
@@ -4,7 +4,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.testng.Assert;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -12,7 +11,6 @@ public class KamaBizbaztiBootApplicationTests {
 
 	@Test
 	public void contextLoads() {
-		Assert.assertEquals(true, true);
 	}
 
 }
