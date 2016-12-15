2713ac63c4299750908b932c8f85260c46a11fa5
shenderov <shenderov.k@gmail.com>
2016-12-15 23:27:10 +0200

    Linux utils

---
+++ src/test/resources/utils/deploy.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+timeout=500
+
+result="Build successfully deployed"
+
+function waiting {
+time=0
+valid_code=200
+while true
+do
+code=$(curl -m 2 -s -o /dev/null -I -w "%{http_code}" -X GET 'http://localhost:8080/kamabizbazti/general/getGeneralChartSelectionsList')
+        if [ "${code}" -ne "${valid_code}" ] && [ "${timeout}" -gt "${time}" ]
+        then
+        count=5
+		time=$((time+count))
+        while [ ${count} -gt 0 ]
+        do
+                echo -n ' .'
+                count=$((count-1))
+                sleep 1
+        done
+		elif [ ${time} -ge ${timeout} ]
+			then
+				echo
+				result="Build failed due to waiting timeout"
+				break
+        else
+				echo
+                break
+        fi
+done
+}
+
+deploy_start=$(date +%s.%N)
+echo "Stop Tomcat"
+sudo -i -u cubie /home/cubie/kamabizbazti/bin/shutdown.sh
+echo "Delete old build"
+sudo -i -u cubie rm -rf /home/cubie/kamabizbazti/webapps/kamabizbazti/
+sudo -i -u cubie rm -f /home/cubie/kamabizbazti/webapps/kamabizbazti
+echo "Copying the new build"
+sudo -i -u cubie cp /var/lib/jenkins/jobs/Kamabizbazti_1_0/workspace/target/kamabizbazti-1.0.0-SNAPSHOT.war /home/cubie/kamabizbazti/webapps/kamabizbazti.war
+echo "Starting Tomcat..."
+sudo -i -u cubie /home/cubie/kamabizbazti/bin/startup.sh
+echo "Waiting for application startup:"
+waiting
+deploy_end=$(date +%s.%N)
+deploy_runtime=$(python -c "print(${deploy_end} - ${deploy_start})")
+echo -n "${result}"
+echo " in $deploy_runtime sec"
---
+++ src/test/resources/utils/run_soapui_sanity.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+#drop all tables
+echo "Dropping database tables"
+mysql -h localhost -u kamabizbazti -pkamabizbazti -e "SET FOREIGN_KEY_CHECKS = 0; DROP TABLE IF EXISTS kamabizbazti.record; DROP TABLE IF EXISTS kamabizbazti.purpose; DROP TABLE IF EXISTS kamabizbazti.user_authority; DROP TABLE IF EXISTS kamabizbazti.user; DROP TABLE IF EXISTS kamabizbazti.selection; DROP TABLE IF EXISTS kamabizbazti.language; DROP TABLE IF EXISTS kamabizbazti.currency; DROP TABLE IF EXISTS kamabizbazti.authority; SET FOREIGN_KEY_CHECKS = 1"
+
+#sleep for 5 seconds
+sleep 5
+
+#deploy
+echo "Deploying build"
+sudo -i -u cubie /home/cubie/tests/deploy.sh
+
+#create test users
+echo "Create test users"
+curl -X POST -H 'Content-Type: application/json' -d '{"email":"usertest@usertest.com","password":"123456","name":"Test User API"}' http://localhost:8080/kamabizbazti/signup
+curl -X POST -H 'Content-Type: application/json' -d '{"email":"usertest2@usertest.com","password":"123456","name":"Test User API"}' http://localhost:8080/kamabizbazti/signup
+echo
+
+#generate purposes and records for users
+echo "Generate purposes and record for test users"
+curl -m 200 -X GET 'http://localhost:8080/kamabizbazti/test/insertData?generateRecordsForAllUsers=true&generatePurposesForAllUsers=true&generateUsers=false'
+echo
+
+#run soapui project
+echo "Running SoapUI sanity project"
+sudo -i -u cubie /home/cubie/SoapUI-5.3.0/bin/testrunner.sh '/var/lib/jenkins/jobs/Kamabizbazti_1_0/workspace/target/test-classes/soapui_sanity.xml'
+
+#clean database afrer sanity
+echo "Clean database after sanity"
+mysql -h localhost -u kamabizbazti -pkamabizbazti -e "DELETE FROM kamabizbazti.record; DELETE FROM kamabizbazti.purpose WHERE type ='CUSTOM'; DELETE FROM kamabizbazti.user_authority; DELETE FROM kamabizbazti.user;"
