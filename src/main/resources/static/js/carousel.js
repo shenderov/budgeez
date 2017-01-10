fa266a549c99874416682f105b5bccac79cc1735
shenderov <shenderov.k@gmail.com>
2017-01-11 00:28:35 +0200

    GUI improvements

--- src/main/resources/static/js/carousel.js
+++ src/main/resources/static/js/carousel.js
@@ -1,6 +1,20 @@
 'use strict';
 
 app.controller('Carousel', function ($scope) {
-    $scope.slides = [];
-
+    $scope.slides = [
+        {
+            "url": "./../../images/slide1.jpg",
+            "header": "Track all your expenses and bills",
+            "text": null
+        },
+        {
+            "url": "./../../images/slide2.jpg",
+            "header": "Get statistics by multiple chart types",
+            "text": null
+        },
+        {
+            "url": "./../../images/slide4.jpg",
+            "header": "Stop waste money and start to save",
+            "text": "test"
+        }]
 });
