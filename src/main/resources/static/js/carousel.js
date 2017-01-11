1854e36666133afab9695ec80fab45a9d322226f
shenderov <shenderov.k@gmail.com>
2017-01-11 09:18:53 +0200

    Security

--- src/main/resources/static/js/carousel.js
+++ src/main/resources/static/js/carousel.js
@@ -3,18 +3,18 @@
 app.controller('Carousel', function ($scope) {
     $scope.slides = [
         {
-            "url": "./../../images/slide1.jpg",
+            "url": "images/carousel/slide1.jpg",
             "header": "Track all your expenses and bills",
             "text": null
         },
         {
-            "url": "./../../images/slide2.jpg",
+            "url": "images/carousel/slide2.jpg",
             "header": "Get statistics by multiple chart types",
             "text": null
         },
         {
-            "url": "./../../images/slide4.jpg",
+            "url": "images/carousel/slide4.jpg",
             "header": "Stop waste money and start to save",
-            "text": "test"
+            "text": null
         }]
 });
