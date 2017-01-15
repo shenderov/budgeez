af7bce36969de0cd265d910fab457839dc16ff05
shenderov <shenderov.k@gmail.com>
2017-01-15 18:41:47 +0200

    Test fix

--- src/test/java/com/kamabizbazti/KamaBizbaztiBootApplicationTests.java
+++ src/test/java/com/kamabizbazti/KamaBizbaztiBootApplicationTests.java
@@ -1,20 +1,14 @@
 package com.kamabizbazti;
 
 import com.kamabizbazti.config.KamaBizbaztiApplicationConfig;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
-import org.testng.annotations.Test;
 
+@SpringBootTest
 @ContextConfiguration(classes = {KamaBizbaztiBootApplication.class, KamaBizbaztiApplicationConfig.class})
 @TestPropertySource(locations="classpath:test.properties")
 public class KamaBizbaztiBootApplicationTests extends AbstractTestNGSpringContextTests {
-
-	@Test
-	public void contextLoads() {
-	}
-
-
-
 
 }
