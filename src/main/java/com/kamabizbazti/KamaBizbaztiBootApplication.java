9b98f3be1bcf1a096fc81a257081bb69be43844e
kostya <shenderov.k@gmail.com>
2017-04-04 23:00:57 +0300

    Config

--- src/main/java/com/kamabizbazti/KamaBizbaztiBootApplication.java
+++ src/main/java/com/kamabizbazti/KamaBizbaztiBootApplication.java
@@ -3,8 +3,10 @@ package com.kamabizbazti;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource(value = "file:/home/kamabizbazti/application.properties", ignoreResourceNotFound = true)
 public class KamaBizbaztiBootApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
