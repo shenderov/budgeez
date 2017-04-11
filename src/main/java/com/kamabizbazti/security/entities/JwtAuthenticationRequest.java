fc330a74913aa8baf5bc3249ec1fa1e5daa4cedb
kostya <shenderov.k@gmail.com>
2017-04-11 21:31:27 +0300

    Fix

--- src/main/java/com/kamabizbazti/security/entities/JwtAuthenticationRequest.java
+++ src/main/java/com/kamabizbazti/security/entities/JwtAuthenticationRequest.java
@@ -36,7 +36,7 @@ public class JwtAuthenticationRequest implements Serializable {
     }
 
     public void setUsername(String username) {
-        this.username = username;
+        this.username = username.toLowerCase();
     }
 
     public String getPassword() {
